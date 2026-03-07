af553485fd8143ec5a8a2bb3cad9ee0b3e55e2b3
WasayRabbani <wasayrabbani69@gmail.com>
2026-03-07 13:40:33 +0500

    pdf_loaded class completed

--- pdf_loader.py
+++ pdf_loader.py
@@ -6,18 +6,27 @@
 5. Return karo     """
 
 
-# import pdf_loader
 
 from pypdf import PdfReader
 
 class PDFLoader:
+    def __init__(self,pdf_link):   #Constructor to automatically give link of pdf
+        self.pdf=pdf_link
     
-    def __init__(self,my_path):
-     self.path=my_path
-     
     def load_pdf(self):
-        reader=PdfReader(self.path)
-        pages=[]
+        
+        loaded_pdf=PdfReader(self.pdf)  # loads pdf from link
+        
+        extracted_text=[]                # makes a list where we will extract all text of pdf
+        
+        for pages in loaded_pdf.pages:  # load all pages from pdf using .pages property
+            extracted_text.append(pages.extract_text())  # saves text from every page to list
+        
+        return extracted_text  # return all extracted text
 
-page=PDFLoader()
-page
+
+
+link=r"D:\IDM Files\sample.pdf"
+p1=PDFLoader(link)
+
+print(p1.load_pdf())
