fd9d154d6d97db2164f1200d56aec8e03464fea7
WasayRabbani <wasayrabbani69@gmail.com>
2026-03-12 16:25:45 +0500

    Fix UI

--- app.py
+++ app.py
@@ -7,36 +7,42 @@ from text_chunker import TextChunker
 from vector_store import VectorStore
 
 
+
 st.title('RAG Document Q&A')
 pdf=st.file_uploader('Upload Document')
 question=st.text_input('Enter Question')
 st.write('Answer will appear here')
 
 if st.button('Search'):
-    st.write('Processing...')
+    if pdf is None:
+        st.warning('Pehle PDF upload karo!')
+    elif not question:
+        st.warning('Write Question!')
+    else:
+        st.write('Processing...')
     
-    
-    load_pdf = PDFLoader(pdf)
-    pdf1=(load_pdf.load_pdf())
+        # Running classes 
+        load_pdf = PDFLoader(pdf)
+        pdf1=(load_pdf.load_pdf())
 
 
-    chunk_text=TextChunker(pdf1)
-    chunks=chunk_text.chunking()
+        chunk_text=TextChunker(pdf1)
+        chunks=chunk_text.chunking()
 
 
-    make_embeddings=EmbeddingGenerator(chunks)
-    embeddings=make_embeddings.embed_text()
- 
-
-    stored_vector=VectorStore(embeddings)
-    whole_index=stored_vector.making_faiss()
-
-
-
-    
-    qh=QueryHandler(question=question,container=whole_index,chunks=chunks)
-    content=qh.get_results()
+        make_embeddings=EmbeddingGenerator(chunks)
+        embeddings=make_embeddings.embed_text()
     
 
-    LLM=LLMHandler(question=question,chunks=content)
-    st.write(LLM.make_chatbot())
+        stored_vector=VectorStore(embeddings)
+        whole_index=stored_vector.making_faiss()
+
+
+
+        
+        qh=QueryHandler(question=question,container=whole_index,chunks=chunks)
+        content=qh.get_results()
+        
+
+        LLM=LLMHandler(question=question,chunks=content)
+        st.write(LLM.make_chatbot())
